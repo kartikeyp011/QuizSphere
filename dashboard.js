5880db82c64a0e7b1f640d8489d0312537b9bec4
kartikeyp011 <kartikeyp011@gmail.com>
2024-07-30 23:47:13 +0530

    Changes dashboard UI and added login/signup UI

---
+++ dashboard.js
@@ -0,0 +1,23 @@
+document.getElementById('q1').onclick = function() {
+    window.location.href = 'knowledge-quiz.html';
+};
+
+document.getElementById('q2').onclick = function() {
+    window.location.href = 'science-quiz.html';
+};
+
+document.getElementById('q3').onclick = function() {
+    window.location.href = 'history-quiz.html';
+};
+
+document.getElementById('q4').onclick = function() {
+    window.location.href = 'entertainment-quiz.html';
+};
+
+document.getElementById('q5').onclick = function() {
+    window.location.href = 'geography.html';
+};
+
+document.getElementById('q6').onclick = function() {
+    window.location.href = 'technology.html';
+};
